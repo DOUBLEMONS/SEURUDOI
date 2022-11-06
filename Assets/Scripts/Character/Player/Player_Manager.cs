b714064077929833cdc9aec5d2e877338a1aa826
김은총 <monmon867867@gmail.com>
2022-11-06 22:29:54 +0900

    개발 시작

---
+++ Assets/Scripts/Character/Player/Player_Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Manager : MonoBehaviour
+{
+    public static Player_Manager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
