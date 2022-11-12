6d14e3e017bcf1d8fb1bc1a8d1d0154f28d944c7
김은총 <monmon867867@gmail.com>
2022-11-12 21:51:44 +0900

    인게임 플레이 UI 배치

--- Assets/Scripts/Character/Player/Attack/Player_Block.cs
+++ Assets/Scripts/Character/Player/Attack/Player_Block.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Blocking : MonoBehaviour
+public class Player_Block : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Character/Player/Attack/Player_Throw.cs
+++ Assets/Scripts/Character/Player/Attack/Player_Throw.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Hitting : MonoBehaviour
+public class Player_Throw : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
